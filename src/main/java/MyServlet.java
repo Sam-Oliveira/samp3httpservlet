818b27e84d9ec281aaa8eb836b42ab1109da0e77
Sam-Oliveira <sdo19@ic.ac.uk>
2021-12-13 14:06:00 +0000

    changed github folder name

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns={"/patients"},loadOnStartup=1)
+@WebServlet(urlPatterns={"","/patients"},loadOnStartup=1)
 public class MyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException{
